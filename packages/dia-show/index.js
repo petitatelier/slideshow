f560d785bb5f44c0fe82881db925dd43e2058353
Yves Lange <yveslange.ch@gmail.com>
2019-03-06 18:34:42 +0100

    Remove imports of controllers in index.js

--- packages/dia-show/index.js
+++ packages/dia-show/index.js
@@ -1,13 +1,11 @@
 import { DiaShow } from "./dia-show.js";
 import { DiaSlide } from "./dia-slide.js";
 import { DiaPo } from "./dia-po.js";
-import { DiaController } from "./dia-controller.js";
 import { DiaDisplaySelector } from "./dia-display-selector.js";
 
 export default {
   DiaShow,
   DiaSlide,
   DiaPo,
-  DiaController,
   DiaDisplaySelector
 }
