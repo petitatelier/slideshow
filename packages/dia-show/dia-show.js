d8bdfe87ea0ff84370b24645b00a8ff42e323d97
Olivier Lange <olange@petit-atelier.ch>
2019-03-09 18:36:44 +0100

    Replaces fullscreen icon character with SVG icon (#3)

--- packages/dia-show/dia-show.js
+++ packages/dia-show/dia-show.js
@@ -43,9 +43,11 @@ export class DiaShow extends LitElement {
         <dia-display-selector .displayList=${this._displayList}></dia-display-selector>
         <button id="cloneWindow" @click=${this._onCloneWindowClicked}>Clone window</button>
         <span slot="after">
-          <button id="fullscreen" @click="${this.fullscreen}">⛶</button>
+          <button id="fullscreen" @click="${this.fullscreen}">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18"
+              ><path d="M4.5 11H3v4h4v-1.5H4.5V11zM3 7h1.5V4.5H7V3H3v4zm10.5 6.5H11V15h4v-4h-1.5v2.5zM11 3v1.5h2.5V7H15V3h-4z"/></svg>
+          </button>
         </span>
-
       </dia-controller>
       <slot></slot>
     `;
